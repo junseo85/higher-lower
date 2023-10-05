0354e540a5df919f1fd2258c0a9c8f91aeca4b81
Jun Seo <112793862+junseo85@users.noreply.github.com>
2023-10-04 23:43:27 -0500

    higher-lower.py

--- higher-lower.py
+++ higher-lower.py
@@ -2,17 +2,18 @@ from art import *
 from game_data import data
 import random
 from replit import clear
-
+#Display art
 print(logo)
-
+#generate random account
 random_int = random.randint(0, len(data) - 1)
 new_random_int = random.randint(0, len(data) - 1)
 a = data[random_int]
 b = data[new_random_int]
 
 score = 0
+#Format the account data into printable format.
 print(f'Compare A: {a["name"]}, a {a["description"]}, from {a["country"]}.')
-
+#Display art
 print(vs)
 print(f'Against B: {b["name"]}, a {b["description"]}, from {b["country"]}.')
 
@@ -20,26 +21,29 @@ print(f'Against B: {b["name"]}, a {b["description"]}, from {b["country"]}.')
 def check_count(a, b, score):
     game = True
     while game == True:
+        #Ask user for a guess.
         answer = input("Who has more followers? Type 'A' or 'B': ").lower()
+        #check if user is correct
         if a["follower_count"] > b["follower_count"] and answer == 'a':
+            #score keeping
             score += 1
-
+            #Making account at position B become the next account at position A
             a = b
+            #generate new account and save to B
             b = data[random.randint(0, len(data) - 1)]
+            #clear the screen
             clear()
             print(logo)
             print(f"You're right! Current score: {score}.")
             print(f'Compare A: {a["name"]}, a {a["description"]}, from {a["country"]}.')
             print(vs)
             print(f'Against B: {b["name"]}, a {b["description"]}, from {b["country"]}.')
-            
+            #print(a["follower_count"], b["follower_count"])
 
 
         elif b["follower_count"] > a["follower_count"] and answer == 'b':
             score += 1
             a = b
-
-            a = b
             b = data[random.randint(0, len(data) - 1)]
             clear()
             print(logo)
@@ -47,7 +51,7 @@ def check_count(a, b, score):
             print(f'Compare A: {a["name"]}, a {a["description"]}, from {a["country"]}.')
             print(vs)
             print(f'Against B: {b["name"]}, a {b["description"]}, from {b["country"]}.')
-            
+            #print(a["follower_count"], b["follower_count"])
 
 
         else:
@@ -58,8 +62,3 @@ def check_count(a, b, score):
 
 
 check_count(a, b, score)
-# from random import randint() to generate random number
-
-# save as random number
-
-# from game_data import data list and use random number to access random information
